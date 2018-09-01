f9c25be3fb05cb7d02bfa0421f0fc316ea700970
Gerardo <gherrer1@binghamton.edu>
2018-09-01 13:50:55 -0400

    Leaderboard is done (just needs styling)

--- app/Components/Leaderboard.js
+++ app/Components/Leaderboard.js
@@ -1,18 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Leaderboard({ loading, error }) {
+function User({ user }) {
+	return (
+		<div className="poll-author">
+			<img src={user.avatarURL} alt={`for ${user.id}`} />
+			<h1>{user.name}</h1>
+			<ul>
+				<li>{user.polls.length} Polls</li>
+				<li>{user.answers.length} Answers</li>
+			</ul>
+		</div>
+	);
+}
+User.propTypes = {
+	user: PropTypes.object.isRequired,
+};
+
+function Leaderboard({ loading, error, users }) {
 	if (loading) {
 		return (<div>Loading</div>);
 	}
 	if (error) {
 		return (<div>Something went wrong. Try again</div>);
 	}
-	return (<div>Done Loading</div>);
+	const sortedUsers = [...users].sort(
+		(u1, u2) => (u1.answers.length + u1.polls.length) < (u2.answers.length + u2.polls.length)
+	);
+	return (
+		<div>
+			{console.log(users) || sortedUsers.map(user => <User user={user} key={user.id} />)}
+		</div>
+	);
 }
 Leaderboard.propTypes = {
 	loading: PropTypes.bool.isRequired,
 	error: PropTypes.bool.isRequired,
+	users: PropTypes.array.isRequired,
 };
 
 export default Leaderboard;
