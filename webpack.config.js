b344e2166dd10fa1556d5361bb0177577ce72da9
Gerardo <gherrer1@binghamton.edu>
2018-08-31 20:40:23 -0400

    Build config and scripts set up, lets start coding

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './app/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'crumple.js'
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: './app/index.html'} )
+    ]
+};
