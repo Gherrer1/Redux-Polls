2b64cef4016c709e5ac1857ccf3fb90b169126cd
Gerardo <gherrer1@binghamton.edu>
2018-08-31 21:03:36 -0400

    Woops missed something in the commit

---
+++ app/Components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function Nav() {
+    return (
+        <nav>
+            <NavLink exact to='/'>Home</NavLink>
+            <NavLink to='/leaderboard'>Leaderboard</NavLink>
+            <NavLink to='/add-poll'>Add Poll</NavLink>
+        </nav>
+    )
+}
+
+export default Nav;
