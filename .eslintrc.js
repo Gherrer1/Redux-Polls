3f6b15e0afbb576b892faec63b335db4313fecb6
Gerardo <gherrer1@binghamton.edu>
2018-08-31 23:08:48 -0400

    New set of linting rules

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": "airbnb",
+    "rules": {
+        "comma-dangle": ["error", {
+            "functions": "ignore",
+            "objects": "always-multiline"
+        }],
+        "indent": ["error", "tab"],
+        "no-param-reassign": [2, {"props": false}],
+        "no-tabs": 0,
+        "react/forbid-prop-types": 0,
+        "react/jsx-filename-extension": 0,
+        "react/jsx-indent": [2, 'tab'],
+        "react/jsx-indent-props": [2, 'tab'],
+        "react/jsx-one-expression-per-line": 0
+    }
+};
