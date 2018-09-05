4d0966f08d78039946fe75502912d1ed3a8eecc6
Gerardo <gherrer1@binghamton.edu>
2018-09-05 02:32:38 -0400

    Made Home component just a better UI

--- app/Components/Home.js
+++ app/Components/Home.js
@@ -2,37 +2,64 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-function Home({
-	loading, error, polls, match,
-}) {
-	if (loading) {
-		return (<div>Loading</div>);
-	}
-	if (error) {
-		return (<div>Something went wrong. Try again</div>);
+class Home extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			showingUnanswered: true,
+		};
+
+		this.showUnanswered = this.showUnanswered.bind(this);
+		this.showAnswered = this.showAnswered.bind(this);
 	}
 
-	const pollToLi = poll => (
-		<li key={poll.id}>
-			<Link to={`${match.url}polls/${poll.id}`}> {poll.question} </Link>
-		</li>
-	);
-	const { unanswered, answered } = polls;
-	const unansweredListItems = unanswered.map(pollToLi);
-	const answeredListItems = answered.map(pollToLi);
+	showUnanswered() {
+		this.setState({
+			showingUnanswered: true,
+		});
+	}
 
-	return (
-		<div>
-			<h1>Unanswered</h1>
-			<ul className="dashboard-list">
-				{unansweredListItems}
-			</ul>
-			<h1>Answered</h1>
-			<ul className="dashboard-list">
-				{answeredListItems}
-			</ul>
-		</div>
-	);
+	showAnswered() {
+		this.setState({
+			showingUnanswered: false,
+		});
+	}
+
+	render() {
+		const {
+			loading, error, polls, match,
+		} = this.props;
+		if (loading) {
+			return (<div>Loading</div>);
+		}
+		if (error) {
+			return (<div>Something went wrong. Try again</div>);
+		}
+
+		const pollToLi = poll => (
+			<li key={poll.id}>
+				<Link to={`${match.url}polls/${poll.id}`}> {poll.question} </Link>
+			</li>
+		);
+		const { unanswered, answered } = polls;
+
+		const { showingUnanswered: showingUnA } = this.state;
+		const list = showingUnA ? unanswered.map(pollToLi) : answered.map(pollToLi);
+
+		return (
+			<div>
+				<div className="dashboard-toggle">
+					<button type="button" style={{ textDecoration: showingUnA ? 'underline' : 'none' }} onClick={this.showUnanswered}>Unanswered</button>
+					<span> | </span>
+					<button type="button" style={{ textDecoration: showingUnA ? 'none' : 'underline' }} onClick={this.showAnswered}>Answered</button>
+				</div>
+				<ul>
+					{list}
+				</ul>
+			</div>
+		);
+	}
 }
 Home.propTypes = {
 	loading: PropTypes.bool.isRequired,
